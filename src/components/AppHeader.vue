ac0595656a35e21af9dcea71979c6f38d6464241
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-07-10 00:17:13 +0200

    feat: add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'AppHeader'
+} 
+</script>
+
+<template>
+    <header>
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg"
+                alt="yugioh logo">
+        </div>
+        <h1>Yu-Gi-Oh Api</h1>
+
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 80px;
+    width: 100%;
+    padding: 20px;
+    display: flex;
+    gap: 30px;
+
+
+}
+</style>
